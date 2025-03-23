985677d747cd6f287dab54ad8ea79d82f42fd553
Jorge Leris <845647@unizar.es>
2025-03-23 13:10:22 +0100

    estructuración

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./src/api/routes/users');
 
 var app = express();
 
@@ -19,12 +17,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: 'Not Found',
+    message: 'The requested resource could not be found',
+    path: req.path
+  });
 });
 
 // error handler
