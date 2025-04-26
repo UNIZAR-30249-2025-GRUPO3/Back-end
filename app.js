522510ad73ee1ab15392736b7f82b597a5012758
jorge1467 <91035401+jorge1467@users.noreply.github.com>
2025-04-26 19:56:52 +0200

    arreglo app.js

--- app.js
+++ app.js
@@ -4,14 +4,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const sessions = require('client-sessions');
 
-
-
 const BD_UserRepository = require('./src/core/infraestructura/Persistencia/BD_UserRepository');
 const UserService = require('./src/core/aplicacion/Servicios/UserService');
 const UserController = require('./src/api/controllers/userController');
 const setupUserRoutes = require('./src/api/routes/userRoutes');
 const { swaggerUi, swaggerDocs } = require("./swagger");
-
 
 // Inicializar dependencias
 const userRepository = new BD_UserRepository();
@@ -20,12 +17,10 @@ const userController = new UserController();
 
 var app = express();
 
-
 app.use(sessions({
   cookieName: 'session',
   secret: 'secret'
 }));
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -52,15 +47,6 @@ app.use(function (err, req, res, next) {
     error: err.message || 'Internal Server Error',
     stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
   });
-});
-
-// Manejo de errores del servidor
-server.on('error', error => {
-  if (error.code === 'EADDRINUSE') {
-    console.error(`El puerto ${PORT} está en uso. Prueba con otro puerto.`);
-  } else {
-    console.error('Error al iniciar el servidor:', error);
-  }
 });
 
 module.exports = app;
