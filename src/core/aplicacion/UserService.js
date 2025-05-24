67b2bbabeb63d9ec3f26645e0aef0ad5806b2df6
jorge1467 <91035401+jorge1467@users.noreply.github.com>
2025-05-25 00:24:38 +0200

    Arreglo roles

--- src/core/aplicacion/UserService.js
+++ src/core/aplicacion/UserService.js
@@ -227,6 +227,7 @@ class UserService {
         rolesToUse = currentRoles;
     }
 
+    updatedData.role = rolesToUse;
     console.log('Roles:', rolesToUse);
     
     // Validación del dominio mediante factoría
