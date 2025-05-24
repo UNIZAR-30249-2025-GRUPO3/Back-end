9b00c80422818b7a2a9bb80533f1658bf9390e96
jorge1467 <91035401+jorge1467@users.noreply.github.com>
2025-05-25 00:00:36 +0200

    arreglo roles

--- src/core/aplicacion/UserService.js
+++ src/core/aplicacion/UserService.js
@@ -209,7 +209,25 @@ class UserService {
       updatedData.department = userObj.department ? userObj.department.name : null;
     }
     
-    const rolesToUse = userData.updateFields.role || currentRoles;
+    let rolesToUse;
+
+    if (userData.updateFields.role) {
+        if (Array.isArray(userData.updateFields.role)) {
+            rolesToUse = userData.updateFields.role;
+        } 
+        else if (userData.updateFields.role.roles) {
+            rolesToUse = Array.isArray(userData.updateFields.role.roles) 
+                      ? userData.updateFields.role.roles 
+                      : [userData.updateFields.role.roles];
+        }
+        else {
+            rolesToUse = [userData.updateFields.role];
+        }
+    } else {
+        rolesToUse = currentRoles;
+    }
+
+    console.log('Roles:', rolesToUse);
     
     // Validación del dominio mediante factoría
     try {
